a6b7c73b872d1686848c965a9ddd9c3d43f7cc12
samuel <samuelrguez97@gmail.com>
2020-08-17 23:56:45 +0200

    renamed folios

---
+++ src/components/common/components/header/Header.styles.ts
@@ -0,0 +1,41 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    block: {
+        boxShadow: '0px 5px 20px 0px grey',
+        position: 'sticky',
+        backgroundColor: '#FFF',
+        top: 0,
+        zIndex: 1000
+    },
+    menu: {
+        width: 250,
+        marginRight: 40,
+        [theme.breakpoints.down('sm')]: {
+            width: 125
+        }
+    },
+    navMenu: {
+        width: 'calc(100% - 290px)',
+        [theme.breakpoints.down('sm')]: {
+            width: 'calc(100% - 165px)'
+        }
+    },
+    pageInfo: {
+        height: '100%',
+        boxShadow: '5px 0px 20px 0px grey'
+    },
+    title: {
+        fontSize: 25
+    },
+    logoImg: {
+        width: 167,
+        height: '100%',
+        [theme.breakpoints.down('sm')]: {
+            width: 117,
+            height: 67
+        }
+    }
+}))
+
+export default useStyles
