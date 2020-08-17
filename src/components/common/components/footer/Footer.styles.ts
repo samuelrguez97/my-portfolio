637a859c3b1d35dac631ff8b193dcfe55771fcca
samuel <samuelrguez97@gmail.com>
2020-08-18 00:08:54 +0200

    added missing

---
+++ src/components/common/components/footer/Footer.styles.ts
@@ -0,0 +1,29 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    footerBlock: {
+        backgroundColor: '#fff',
+        width: '100%',
+        boxShadow: '0px 3px 20px 0px'
+    },
+    footerContainer: {
+        width: '100%',
+        height: '100%',
+        color: '#5656ef',
+        fontSize: 20,
+        [theme.breakpoints.down('sm')]: {
+            fontSize: 18
+        }
+    },
+    logoImg: {
+        width: 117,
+        height: 67
+    },
+    footerItem: {
+        [theme.breakpoints.down('sm')]: {
+            margin: '10px 0'
+        }
+    }
+}))
+
+export default useStyles
