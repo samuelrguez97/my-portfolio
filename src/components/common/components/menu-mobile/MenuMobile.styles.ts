343ce81fee53e93ba7feb19451db766fefdbccfa
samuel <samuelrguez97@gmail.com>
2021-02-03 20:19:16 +0100

    fix

--- src/components/common/components/menu-mobile/MenuMobile.styles.ts
+++ src/components/common/components/menu-mobile/MenuMobile.styles.ts
@@ -1,5 +1,4 @@
 import { makeStyles } from "@material-ui/core/styles";
-import zIndex from "@material-ui/core/styles/zIndex";
 
 const useStyles = makeStyles((theme) => ({
   "@keyframes slide-in": {
